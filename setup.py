9b5308721ea91e3302bb94c5db87797da7cf7b2d
Aardiaan <adriancudecki@gmail.com>
2024-06-08 22:14:36 +0200

    potęgowanie i przyciski do funkcji

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "Kalkulator",
-    version = "1.2",
+    version = "1.3",
     description = "Kalkulator z funkcją przeliczania na liczby binarne",
     executables = [Executable("app1.py", base = "Win32GUI")]
 )
