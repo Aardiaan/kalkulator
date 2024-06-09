a90ef4c0878f5cedbacf12f9ba273163c2955d1b
Aardiaan <adriancudecki@gmail.com>
2024-06-09 15:17:18 +0200

    cxfreeze setup 1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "Kalkulator",
-    version = "1.3",
+    version = "1.4",
     description = "Kalkulator z funkcją przeliczania na liczby binarne",
     executables = [Executable("app1.py", base = "Win32GUI")]
 )
